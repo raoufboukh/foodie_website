6e84dba855de97bcf8adccaa820e5741ab47e03e
raoufboukh <zakib2316@gmail.com>
2025-10-02 16:59:23 +0100

    fix: correct padding class and adjust margin for app download buttons in Hero component

--- client/components/Home/Hero.tsx
+++ client/components/Home/Hero.tsx
@@ -5,7 +5,7 @@ import { BsSend } from "react-icons/bs";
 
 const Hero = () => {
   return (
-    <div className="pt-22">
+    <div className="py-22">
       <div className="container flex justify-between flex-wrap items-center lg:!px-24">
         <div className="lg:basis-[45%] basis-[80%] pt-5 mx-auto lg:mx-0">
           <h2 className="sm:text-5xl text-3xl leading-tight font-bold text-center lg:text-left">
@@ -33,14 +33,14 @@ const Hero = () => {
               Apps Available to download on
             </p>
             <div className="flex gap-2 items-center">
-              <div className="flex items-center gap-2 sm:text-sm text-xs rounded-md border border-gray-400 px-2 py-1 cursor-pointer hover:shadow-lg transition-shadow duration-300 mx-auto">
+              <div className="flex items-center gap-2 sm:text-sm text-xs rounded-md border border-gray-400 px-2 py-1 cursor-pointer hover:shadow-lg transition-shadow duration-300 sm:mx-0 mx-auto">
                 <FaGooglePlay className="text-xl text-black" />
                 <p>
                   Download on the{" "}
                   <span className="font-bold block">Google Play</span>
                 </p>
               </div>
-              <div className="flex items-center gap-2 sm:text-sm text-xs rounded-md border border-gray-400 px-2 py-1 cursor-pointer hover:shadow-lg transition-shadow duration-300 mx-auto">
+              <div className="flex items-center gap-2 sm:text-sm text-xs rounded-md border border-gray-400 px-2 py-1 cursor-pointer hover:shadow-lg transition-shadow duration-300 sm:mx-0 mx-auto">
                 <FaApple className="text-xl text-black" />
                 <p>
                   Download on the{" "}
